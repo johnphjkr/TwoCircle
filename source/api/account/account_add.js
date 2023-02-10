c98bb8b7a545cb64ffa98aa18e6787728c6d15b6
xiuxiuboK <tjsalsun@gmail.com>
2023-02-10 17:41:50 +0900

    feat: Add addAccount func In account_add.js
    - addAccount 함수 생성

--- source/api/account/account_add.js
+++ source/api/account/account_add.js
@@ -1,1 +1,18 @@
 import { headers, url } from "../requests.js";
+
+export async function addAccount(data) {
+    const res = await fetch(url + "/account ", {
+      method: "POST",
+      headers: {
+        ...headers,
+        authorization: `Bearer` + Response.headers.getItem("accessToken"),
+      },
+      body: JSON.stringify({
+        bankCode: data.bankCode,
+        accountNumber: data.accountNumber,
+        phoneNumber:data.phoneNumber,
+        signature:data.signature
+      }),
+    });
+    return res.json();
+  }
