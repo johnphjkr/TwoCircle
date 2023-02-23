c3472307a2bcb5c386e1d7883fe9f0f76cc69b54
PHJ <johnphjkr@gmail.com>
2023-02-23 22:18:04 +0900

    feat: add date filter in purchase history 기간에 따른 필터 적용

--- source/api/products/user/purchase_cancel_api.js
+++ source/api/products/user/purchase_cancel_api.js
@@ -5,7 +5,7 @@ export async function purchaseCancel(data) {
         method: "POST",
         headers: {
             ...headers,
-            authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU0WnFvSFRhVERBbmlrWkdaQlYyIiwiaWF0IjoxNjc2ODgyODQ2LCJleHAiOjE2NzY5NjkyNDYsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.OhLbtBEHEIz8PO42sHvWeP7fx_fJKCf8iXr_uZn8LaA`,
+            authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU0WnFvSFRhVERBbmlrWkdaQlYyIiwiaWF0IjoxNjc3MTQyODU0LCJleHAiOjE2NzcyMjkyNTQsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.inBksaxKm8DBy8zfCcoRw5rgrJ9BGyMdDQ-sfunPBhs`,
         },
         body: JSON.stringify({
             detailId: data.detailId
