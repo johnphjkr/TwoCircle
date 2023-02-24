68e460b0af183c915e56a9e4a699dd436da17d3b
Nevacat <nevacattery217@naver.com>
2023-02-24 23:35:38 +0900

    feat: Build Up Content Of Route
    router before를 추가했습니다.
    또한, 장바구니. 찜목록 등 헤더 메뉴의 라우터를 연결했습니다.

--- source/route.js
+++ source/route.js
@@ -1,15 +1,30 @@
-
-
 import Navigo from "navigo";
+
+import { authCheck } from "./api/certified/authcheck_api.js";
+
+// 페이지
 import { mainRender } from "../pages/user/main.js";
 import { loginRender } from "../pages/user/login.js";
 import { signupRender } from "../pages/user/signup";
+import { navRender, mypageRender } from "../pages/user/mypage";
+import { wishRender } from "../pages/user/wish_list"
+import { pruchaseRender } from "../pages/user/purchase_history"
+import { cartRender } from "../pages/user/cart";
 // import { productDetailRender } from "./js/product_details.js";
-import { paymentRender } from "./js/payment.js";
+// import { paymentRender } from "./js/payment.js";
+
 
 const router = new Navigo("/");
 
-router.hooks({
+router.hooks({ 
+  before: async (done,match) =>{
+    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+    const auth = await authCheck(accessToken);
+    if(auth) {
+      console.log('[auth sucess]',{user: auth})
+    }
+    done()
+  },
   after: (match) => {
     window.scroll(0,0);
   }
@@ -19,12 +34,21 @@ router.on(
     "/" : () => {
       mainRender();
     },
-    "/login": () => {loginRender()},
-    "/signup" : () => {signupRender()}
+    "login": () => {loginRender()},
+    "signup" : () => {signupRender()},
+    "cart" : () => {cartRender()},
+    "mypage": () => {
+      navRender()
+      mypageRender()
+    },
+    "mypage/wish": () => {
+      navRender()
+      wishRender()
+    },
+    "mypage/purchase" : () => {
+      pruchaseRender()
+    }
 }).resolve();
-// router.on("/product/detail/:productId", function (data) {
-//   productDetailRender(data);
-// });
-// router.resolve();
+
 
 export {router}
