7af338db6b7399086eae6cb4939da32e5e136dc5
Nevacat <nevacattery217@naver.com>
2023-02-27 19:37:29 +0900

    feat:Edit Content Of Page Routing And Page Guard
    1. 로그인이 되어있지 않는다면, 로그인 정보가 필요한 곳은 들어갈 수
       없도록 구현했습니다.
    
    2. 로그인 후 해당 계정의 엑세스 토큰이 만료되면 로컬에서 지워지도록
       설정했습니다.
    
    3. nav태그 및 검색에 따른 상품이 렌더링 되도록 구현했습니다.
    
    4. 또한 라우터가 필요한 구역의 라우터 작업을 하였습니다.

--- source/route.js
+++ source/route.js
@@ -7,48 +7,80 @@ import { mainRender } from "../pages/user/main.js";
 import { loginRender } from "../pages/user/login.js";
 import { signupRender } from "../pages/user/signup";
 import { navRender, mypageRender } from "../pages/user/mypage";
-import { wishRender } from "../pages/user/wish_list"
-import { pruchaseRender } from "../pages/user/purchase_history"
+import { wishRender } from "../pages/user/wish_list";
+import { pruchaseRender } from "../pages/user/purchase_history";
 import { cartRender } from "../pages/user/cart";
-// import { productDetailRender } from "./js/product_details.js";
-// import { paymentRender } from "./js/payment.js";
+import { accountRender } from "../pages/user/account";
+import { productListRender } from "../pages/user/product_list.js";
+import { productRender } from "./js/product_list.js";
+export const router = new Navigo("/");
 
-
-const router = new Navigo("/");
-
-router.hooks({ 
-  before: async (done,match) =>{
-    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+router.hooks({
+  before: async (done, match) => {
+    const accessToken = JSON.parse(localStorage.getItem("accessToken"));
     const auth = await authCheck(accessToken);
-    if(auth) {
-      console.log('[auth sucess]',{user: auth})
+    const onlyUserPages = ["mypage", "mypage/wish", "cart", "account"];
+    if (onlyUserPages.includes(match.url) && !auth) {
+      router.navigate("login");
+      done();
     }
-    done()
+    if ((match.url === "login" || match.url === "signup") && auth) {
+      router.navigate("");
+      done();
+    }
+    done();
   },
   after: (match) => {
-    window.scroll(0,0);
-  }
-})
-router.on(
-  {
-    "/" : () => {
+    window.scroll(0, 0);
+    console.log(match.user);
+  },
+});
+router
+  .on({
+    "/": () => {
       mainRender();
     },
-    "login": () => {loginRender()},
-    "signup" : () => {signupRender()},
-    "cart" : () => {cartRender()},
+    "login": () => {
+      loginRender();
+    },
+    "signup": () => {
+      signupRender();
+    },
+    "cart": () => {
+      cartRender();
+    },
     "mypage": () => {
-      navRender()
-      mypageRender()
+      navRender();
+      mypageRender();
     },
     "mypage/wish": () => {
-      navRender()
-      wishRender()
+      navRender();
+      wishRender();
     },
-    "mypage/purchase" : () => {
-      pruchaseRender()
+    "mypage/purchase": () => {
+      navRender();
+      pruchaseRender();
+    },
+    "mypage/account": () => {
+      navRender();
+      accountRender();
+    },
+    "product_list/:id": (match) => {
+      
+      productListRender(match.data.id)
+      productRender("",[match.data.id])
+    },
+    "product_search/:id":(match) => {
+      productListRender(match.data.id)
+      productRender(match.data.id,[])
     }
-}).resolve();
+  })
+  .resolve();
 
+const search = document.querySelector(".search");
+const searchInput = document.querySelector(".search input")
 
-export {router}
+search.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  router.navigate(`product_search/${searchInput.value}`)
+})
