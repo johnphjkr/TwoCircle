e24e65b2a477197bb84627e5f058b76a5ae050bb
xiuxiuboK <tjsalsun@gmail.com>
2023-03-01 19:36:27 +0900

    fix: Edit func in cart.js
    - clickHandler 이벤트 핸들러 함수 생성
    - 수량 증감 버튼 누를시 decrease/ increase 함수 생성
    - 원화랑 단위 표시 재수정

--- source/js/cart.js
+++ source/js/cart.js
@@ -1,8 +1,6 @@
-
-
 /**장바구니 로컬스토리지에 저장된 데이터 갖고오는 변수*/
 
-export function cartHandler(){
+export function cartHandler() {
   const ulEl = document.querySelector(".cart_list");
   const totalCheckbox = document.querySelector(".info_total_checkbox");
   const cancelBtn = document.querySelector(".cancel_btn");
@@ -10,21 +8,23 @@ export function cartHandler(){
   const basketItem = JSON.parse(localStorage.getItem("basket"));
   cancelBtn.addEventListener("click", () => {
     let cartItemList = [...basketItem];
-  
+
     const selectedItemList = Array.from(
       document.querySelectorAll(".want_checkbox:checked")
-    ).map((selectedInput) => selectedInput.closest(".cart_list_item").dataset.id);
+    ).map(
+      (selectedInput) => selectedInput.closest(".cart_list_item").dataset.id
+    );
     selectedItemList.forEach((id) => {
       cartItemList = cartItemList.filter((cartItem) => cartItem.id !== id);
     });
-  
+
     localStorage.setItem("basket", JSON.stringify(cartItemList));
   });
-  
+
   totalCheckbox.addEventListener("change", (e) => {
     const checkBox = ulEl.querySelectorAll(".want_checkbox");
     const { checked } = e.target;
-  
+
     checkBox.forEach((item) => {
       if (checked) {
         item.checked = true;
@@ -33,30 +33,25 @@ export function cartHandler(){
       }
     });
   });
-  
+
   purchaseBtn.addEventListener("click", () => {
     let cartItemList = [...basketItem];
-  
+
     const cartAmount = Array.from(
       document.querySelectorAll(".cart_card .amount")
     ).map((count) => count.textContent);
     const cartPrice = Array.from(
       document.querySelectorAll(".cart_card .price")
     ).map((price) => price.textContent);
-  
-  
-      for(let i = 0; i < cartItemList.length; i++) {
-        
-        cartItemList[i].price = cartPrice[i]
-        cartItemList[i].count = cartAmount[i]
-        
-      }
-    
+
+    for (let i = 0; i < cartItemList.length; i++) {
+      cartItemList[i].price = cartPrice[i];
+      cartItemList[i].count = cartAmount[i];
+    }
+
     localStorage.setItem("basket", JSON.stringify(cartItemList));
   });
 }
-
-
 
 /**장바구니 리스트 렌더링 함수 */
 export const renderCartList = () => {
@@ -90,47 +85,21 @@ export const renderCartList = () => {
                         </div>
 
                         <div class="price_wrap">
-                          <span class="price">${item.price}원</span>
+                          <span class="price">${item.totalPrice.toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
                         </div>
                       </div>                            
         `;
 
-    const decreaseBtn = liEl.querySelector(".decrease_btn");
-    const increaseBtn = liEl.querySelector(".increase_btn");
-    const amountEl = liEl.querySelector(".amount");
-    const priceEl = liEl.querySelector(".price");
-    let amount = item.count;
-    const price = item.price;
 
-    decreaseBtn.addEventListener("click", () => {
-      if(amount >1) {
-
-        amount -= 1;
-        amountEl.textContent = amount;
-        priceEl.textContent = amount * price;
-        getToTalPrice();
-        
-      }
-    
-    });
-
-    increaseBtn.addEventListener("click", () => {
-      console.log(amount)
-      amount += 1;
-      amountEl.textContent = amount;
-      priceEl.textContent = amount * price;
-      
-      getToTalPrice();
-      
-    });
 
     return liEl;
   });
 
-  
   renderTotalPrice();
   ulEl.innerHTML = "";
   ulEl.append(...liEls);
+  ulEl.addEventListener('click',clickHandler)
 };
 
 
@@ -157,25 +126,85 @@ const renderTotalPrice = () => {
   getToTalPrice();
 };
 
-
-
 /** 장바구니 총 합계 구하는 함수*/
 function getToTalPrice() {
-  const ulEl = document.querySelector(".cart_list");
+  const basketItem = JSON.parse(localStorage.getItem("basket"));
+
+  const initPrice = basketItem.reduce((prev, cur) => {
+    return prev + cur.totalPrice;
+  }, 0);
+
   const totalPriceArea = document.querySelector(".cart_total_price_area");
-  let total = 0;
+  let total = initPrice;
   let orderToTalPrice = totalPriceArea.querySelector(
     ".cart_order_price .price"
   );
   let cartToTalPrice = totalPriceArea.querySelector(".cart_total_price .price");
 
-  const liEls = ulEl.querySelectorAll(".cart_list_item  .price");
-
-  for (let price = 0; price < liEls.length; price++) {
-    let priceText = Number(liEls[price].textContent.replace(',',''));
-    total += priceText;
-  }
-  orderToTalPrice.textContent = total;
-  cartToTalPrice.textContent = total;
+  orderToTalPrice.textContent = `${total.toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`;
+  cartToTalPrice.textContent = `${total.toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`;
 }
 
+
+const  clickHandler = (e) => {
+ const liEls = e.target.closest('.cart_list > li')
+ if(!liEls) {
+  return 
+ }
+
+ if(e.target.matches('.decrease_btn')) {
+    decrease(liEls)
+ }
+ if(e.target.matches('.increase_btn')) {
+    increase(liEls)
+ }
+}
+
+const decrease = (liEls) => {
+  
+  const basketItem = JSON.parse(localStorage.getItem("basket"));
+  const targetId = liEls.dataset.id;
+  const targetItem = basketItem.find(item => item.id === targetId)
+  const decreaseBtn = liEls.querySelector(".decrease_btn")
+  const amountEl = liEls.querySelector(".amount");
+  const priceEl = liEls.querySelector(".price");
+  let amount = targetItem.count 
+  
+
+
+     if (amount > 1) {
+        amount -= 1;
+        amountEl.textContent = amount;
+        priceEl.textContent = `${(amount * (targetItem.price-targetItem.price*Number(targetItem.discountRate)/100)).toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`;
+        targetItem.count = amount 
+        targetItem.totalPrice =  targetItem.count * (targetItem.price-targetItem.price*Number(targetItem.discountRate)/100);
+        localStorage.setItem('basket',JSON.stringify(basketItem))  
+        
+     }
+     renderTotalPrice()
+  
+}
+const increase = (liEls) => {
+  
+  const basketItem = JSON.parse(localStorage.getItem("basket"));
+  const targetId = liEls.dataset.id;
+  const targetItem = basketItem.find(item => item.id === targetId)
+  const increaseBtn = liEls.querySelector(".increase_btn")
+  const amountEl = liEls.querySelector(".amount");
+  const priceEl = liEls.querySelector(".price");
+  let amount = targetItem.count 
+
+      
+      amount += 1;
+      amountEl.textContent = amount;
+      priceEl.textContent = `${(amount * (targetItem.price-targetItem.price*Number(targetItem.discountRate)/100)).toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`;
+      targetItem.count = amount 
+      targetItem.totalPrice = targetItem.count * (targetItem.price-targetItem.price*Number(targetItem.discountRate)/100);
+      localStorage.setItem('basket',JSON.stringify(basketItem)) 
+  
+      renderTotalPrice()
+}
