2b28e277480c9cf562329ad17f4b93adca1175bd
xiuxiuboK <tjsalsun@gmail.com>
2023-03-03 00:35:31 +0900

    fix: 조건부 렌더링
    -찜리스트 없을시 해당 부분 렌더링

--- pages/user/wish_list.js
+++ pages/user/wish_list.js
@@ -1,9 +1,11 @@
 import { renderWishList } from "../../source/js/wish_list"
 
-const wishList = JSON.parse(localStorage.getItem("wish"));
 
 export function wishRender() {
+  const wishList = JSON.parse(localStorage.getItem("wish"));
   const mypage = document.querySelector("#mypage")
+  const isEmpty = wishList === null || wishList.length === 0
+  
   mypage.innerHTML=/*html*/`
   <section class="wish_list">
     <div class="inner wish_inner">
@@ -11,8 +13,17 @@ export function wishRender() {
         <div class="wish_header">
           <h2 class="header">찜 리스트</h2>
         </div>
-        <ul class="list">
-        </ul>
+        ${isEmpty?/*html*/`<div class ="wish_none">
+          <div class="wish_icon">
+          <img class="heart_img" src="../../image/heart_img.png"/>
+          </div>  
+          <div class ="empty_text">찜 리스트가 비었습니다.</div>
+          <div class="wish_btn">
+            <a href="/" data-navigo >계속 쇼핑하기</a>
+          </div>
+        </div>`:/*html*/`<ul class="list">
+        </ul>`}
+        
       </div>
     </div>
   </section>`
