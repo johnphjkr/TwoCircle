51610fdfb7106134a3eb8c12315ee246629d6eb6
xiuxiuboK <tjsalsun@gmail.com>
2023-03-03 15:58:09 +0900

    docs: Add className
    - innerHTML 클래스네임 추가

--- pages/user/wish_list.js
+++ pages/user/wish_list.js
@@ -1,19 +1,21 @@
-import { renderWishList } from "../../source/js/wish_list"
-
+import { renderWishList } from "../../source/js/wish_list";
 
 export function wishRender() {
   const wishList = JSON.parse(localStorage.getItem("wish"));
-  const mypage = document.querySelector("#mypage")
-  const isEmpty = wishList === null || wishList.length === 0
-  
-  mypage.innerHTML=/*html*/`
+  const mypage = document.querySelector("#mypage");
+  const isEmpty = wishList === null || wishList.length === 0;
+
+  mypage.innerHTML = /*html*/ `
   <section class="wish_list">
     <div class="inner wish_inner">
-      <div class="wish_wrap">
+      <div class="wish_wrap products">
         <div class="wish_header">
           <h2 class="header">찜 리스트</h2>
         </div>
-        ${isEmpty?/*html*/`<div class ="wish_none">
+        ${
+          isEmpty
+            ? /*html*/ `
+        <div class ="wish_none">
           <div class="wish_icon">
           <img class="heart_img" src="../../image/heart_img.png"/>
           </div>  
@@ -21,11 +23,16 @@ export function wishRender() {
           <div class="wish_btn">
             <a href="/" data-navigo >계속 쇼핑하기</a>
           </div>
-        </div>`:/*html*/`<ul class="list">
-        </ul>`}
+        </div>
+        `
+            : /*html*/ `
+        <ul class="list products_lists">
+        </ul>
+        `
+        }
         
       </div>
     </div>
-  </section>`
-  renderWishList(wishList)
+  </section>`;
+  renderWishList(wishList);
 }
