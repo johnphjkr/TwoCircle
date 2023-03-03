b61f6fb6fd408d4fabc12886590bd14e485a5334
jiyoon29 <yoon__29@naver.com>
2023-03-03 21:29:26 +0900

    feat: product_add page design html
    - product_add page 디자인과 구조 수정

--- pages/admin/product_add.js
+++ pages/admin/product_add.js
@@ -8,37 +8,51 @@ export function adminProductAdd() {
         <h2>상품추가</h2>
       </div>
       <div class="admin_wrap">
-        <div>
-          <p>제품이름</p>
-          <input type="text" class="product_name" />
+        <div class="admin_product_add">
+          <div>
+            <p>제품이름</p>
+            <input type="text" class="product_name" />
+          </div>
+          <div>
+            <p>제품가격</p>
+            <input type="text" class="product_price" />
+          </div>
+          <div>
+            <p>제품 상세 설명</p>
+            <input type="text" class="product_text" />
+          </div>
+          <div>
+            <p>태그</p>
+            <input type="text" class="product_tag" />
+            <span>태그는 ', '로 구분해주세요</span>
+          </div>
+          <div>
+            <p>제품 사진</p>
+            <div class="button thumbnail_btn">
+              <p>찾아보기</p>
+            </div>
+            <input type="file" class="product_thumbnail" />
+            <span>1MB 이하의 사진만 가능합니다.</span>
+          </div>
+          <div>
+            <p>제품 상세 사진</p>
+            <div class="button photo_btn">
+              <p>찾아보기</p>
+            </div>
+            <input type="file" class="product_photo" />
+            <span>4MB 이하의 사진만 가능합니다.</span>
+          </div>
+          <div>
+            <p>제품 할인율</p>
+            <input type="text" class="product_discount" />
+            <span class="percent">%</span>
+          </div>
+          <button type="button" class="product_registration">제품등록</button>
         </div>
-        <div>
-          <p>제품가격</p>
-          <input type="text" class="product_price" />
+        <div class="product_add_img">
+          <div class="thumbnail_img"></div>
+          <div class="photo_img"></div>
         </div>
-        <div>
-          <p>제품 상세 설명</p>
-          <input type="text" class="product_text" />
-        </div>
-        <div>
-          <p>태그</p>
-          <input type="text" class="product_tag" />
-        </div>
-        <div>
-          <p>제품 사진</p>
-          <input type="file" class="product_thumbnail" />
-          <div class="img"></div>
-        </div>
-        <div>
-          <p>제품 상세 사진</p>
-          <input type="file" class="product_photo" />
-          <div class="detail_img"></div>
-        </div>
-        <div>
-          <p>제품 할인율</p>
-          <input type="text" class="product_discount" />
-        </div>
-        <button type="button" class="product_registration">제품등록</button>
       </div>
     </div>`;
   productAddItem();
