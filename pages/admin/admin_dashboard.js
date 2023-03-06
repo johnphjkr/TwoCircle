1f2c536045724ebae23336259fcfc5bdf8da8e58
hyeon17 <janghyeon8725@naver.com>
2023-03-07 00:39:48 +0900

    feat: 관리자 대시보드 태그별 수량 표시 추가

--- pages/admin/admin_dashboard.js
+++ pages/admin/admin_dashboard.js
@@ -1,6 +1,6 @@
 import { dashBoardHandler } from "../../source/js/admin/admin_dashboard.js";
 
-export async function dashBoardRender() {
+export function dashBoardRender() {
   const admin = document.querySelector("#admin");
   admin.innerHTML = /* html */ `
   <div class="container">
@@ -9,7 +9,7 @@ export async function dashBoardRender() {
     </div>
     <div class="admin_dashboard">
       <div>
-        <canvas id="myChart" width="650px" height="300px"></canvas>
+        <canvas id="myChart" width="700px" height="200px"></canvas>
       </div>
     </div> 
   </div>
--- source/js/admin/admin_dashboard.js
+++ source/js/admin/admin_dashboard.js
@@ -1,44 +1,65 @@
-import { allProduct } from "../../api/products/admin/allProduct_api.js";
+import {
+  productDataHandler,
+  transactionDataHandler,
+} from "../admin/admin_products_data.js";
+
 
 export async function dashBoardHandler() {
-   const config = {
-     type: "bar",
-     data: data,
-     options: {
-       scales: {
-         y: {
-           beginAtZero: true,
-         },
-       },
-     },
-  };
-  const labels = Utils.months({ count: 7 });
+  const { sunGlass, goggles, glassesFrame, best, newItem, md } = await productDataHandler();
+  // const transaction = await transactionDataHandler();
+
+  // 차트를 그릴 캔버스 요소
+  const canvas = document.querySelector("#myChart");
+
+  // 데이터
   const data = {
-    labels: labels,
+    labels: ["선글라스", "고글", "안경테", "베스트", "신상품", "MD상품"],
     datasets: [
       {
-        label: "My First Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40],
+        label: "수량",
+        data: [
+          sunGlass.length,
+          goggles.length,
+          glassesFrame.length,
+          best.length,
+          newItem.length,
+          md.length,
+        ],
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
           "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
       },
     ],
   };
+
+  // 차트 객체 생성
+  const myChart = new Chart(canvas, {
+    type: "bar",
+    data: data,
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
