68d4d167d66cf24470b2c1970f6c8e625a659624
hyeon17 <janghyeon8725@naver.com>
2023-03-10 00:21:36 +0900

    feat: 요약 정보, 상품 판매 현황 리스트 추가

--- source/js/admin/admin_dashboard.js
+++ source/js/admin/admin_dashboard.js
@@ -1,12 +1,17 @@
 import {
-  productDataHandler,
-  transactionDataHandler,
+  productDataHandler
 } from "../admin/admin_products_data.js";
-
+import { transactionDetail } from "../../api/products/admin/product_transactions_api.js";
+import { allProduct } from "../../api/products/admin/allProduct_api.js";
+import { userlist } from "../../api/products/admin/user_list_api.js";
 
 export async function dashBoardHandler() {
-  const { sunGlass, goggles, glassesFrame, best, newItem, md } = await productDataHandler();
-  // const transaction = await transactionDataHandler();
+  const { sunGlass, goggles, glassesFrame, best, newItem, md } =
+    await productDataHandler();
+  const products = await allProduct();
+  const users = await userlist();
+  const items = await transactionDetail();
+
 
   // 차트를 그릴 캔버스 요소
   const canvas = document.querySelector("#myChart");
@@ -62,4 +67,99 @@ export async function dashBoardHandler() {
       },
     },
   });
+
+  let index = 0;
+  let sum = 0;
+  const liEl = items.map((item) => {
+    const listEl = document.createElement("div");
+    listEl.classList.add("list");
+    const numberEl = document.createElement("div");
+    const dateEl = document.createElement("div");
+    const userEl = document.createElement("div");
+    const bankEl = document.createElement("div");
+    const salepriceEl = document.createElement("div");
+    const itemEl = document.createElement("div");
+    const cancelpriceEl = document.createElement("div");
+    const totalpriceEl = document.createElement("div");
+
+    numberEl.classList.add("content_number");
+    dateEl.classList.add("content_date");
+    userEl.classList.add("content_user");
+    bankEl.classList.add("content_bank");
+    salepriceEl.classList.add("content_saleprice");
+    itemEl.classList.add("content_item");
+    cancelpriceEl.classList.add("content_cancelprice");
+    totalpriceEl.classList.add("content_totalprice");
+
+    numberEl.innerHTML = `<div>${index + 1}</div>`;
+    const formattedTimePaid = item.timePaid.replace(
+      /T\d{2}:\d{2}:\d{2}\.\d{3}Z/,
+      " "
+    );
+    dateEl.innerHTML = `<div>${formattedTimePaid}</div>`;
+    userEl.innerHTML = `<div>${item.user.email}</div>`;
+    bankEl.innerHTML = `<div>${item.account.bankName}</div>`;
+
+    const originalPrice = item.product.price;
+    const discountRate = item.product.discountRate;
+    const discountedPrice = discountRate
+      ? originalPrice * ((100 - discountRate) * 0.01)
+      : originalPrice;
+    const formattedDiscountedPrice = discountedPrice
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    salepriceEl.innerHTML = `<div>${formattedDiscountedPrice}원</div>`;
+
+    const formattedTitle = item.product.title.replace(/\/.*/, "");
+    itemEl.innerHTML = `<div>${formattedTitle}</div>`;
+
+    if (item.isCanceled) {
+      salepriceEl.innerHTML = "<div>0원</div>";
+      const cancelPrice = discountRate
+        ? -1 * originalPrice * ((100 - discountRate) * 0.01)
+        : -1 * originalPrice;
+      const formattedCancelPrice = cancelPrice
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      cancelpriceEl.innerHTML = `<div>${formattedCancelPrice}원</div>`;
+      totalpriceEl.innerHTML = `<div>${formattedCancelPrice}원</div>`;
+      totalpriceEl.style.color = "blue";
+      sum += cancelPrice;
+    } else {
+      cancelpriceEl.innerHTML = "<div>0원</div>";
+      totalpriceEl.innerHTML = `+<div>${formattedDiscountedPrice}원</div>`;
+      totalpriceEl.style.color = "red";
+      sum += discountedPrice;
+    }
+
+    if (item.user.email !== "admin@naver.com") {
+      listEl.append(
+        numberEl,
+        dateEl,
+        userEl,
+        bankEl,
+        salepriceEl,
+        itemEl,
+        cancelpriceEl,
+        totalpriceEl
+      );
+      index++;
+    } else {
+      return [];
+    }
+    return listEl;
+  });
+
+  const tableEl = document.querySelector(".table_content");
+  tableEl.append(...liEl);
+
+
+    const itemcontEl = document.querySelector(".summary_itemcount");
+    const salesumEl = document.querySelector(".summary_salesum");
+    const memberEl = document.querySelector(".summary_member");
+  itemcontEl.append(`판매 상품 갯수 : ${products.length}개`);
+  salesumEl.append(`총 판매 매출 : ${sum
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`);
+  memberEl.append( `회원 수 : ${users.length}명`);
 }
