f37a7ec8ef381bcc2946a82e149d1fe3fc743c36
hyeon17 <janghyeon8725@naver.com>
2023-03-15 13:16:46 +0900

    fix: 카카오톡 공유 링크 수정

--- source/js/product_details.js
+++ source/js/product_details.js
@@ -174,16 +174,16 @@ export async function productDetailHandler(id) {
         imageUrl: "https://avatars.githubusercontent.com/u/124231330?s=200&v=4",
         link: {
           // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-          mobileWebUrl: "http://localhost:1234",
-          webUrl: "http://localhost:1234",
+          mobileWebUrl: "https://magical-babka-4591f3.netlify.app",
+          webUrl: "https://magical-babka-4591f3.netlify.app",
         },
       },
       buttons: [
         {
           title: "웹사이트로 이동",
           link: {
-            mobileWebUrl: "http://localhost:1234",
-            webUrl: "http://localhost:1234",
+            mobileWebUrl: "https://magical-babka-4591f3.netlify.app",
+            webUrl: "https://magical-babka-4591f3.netlify.app",
           },
         },
       ],
